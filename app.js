ad0e4edc9a1f9494df538f5d932b3e6d4a52143b
JoseGabriel-web <josegabriel.mercedes@yahoo.com>
2020-07-08 20:02:12 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -42,11 +42,8 @@ add.addEventListener('click', ()=>{
 
 
 document.querySelector('#text').addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
-    // Cancel the default action, if needed
     event.preventDefault();
-    // Trigger the button element with a click
     document.getElementById("add-btn").click();
   }
 });
